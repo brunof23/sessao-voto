69f8c06e2c000339326e31645c3dd62b46955787
Bruno <brunoferreira.tecnico@gmail.com>
2023-10-31 22:42:48 -0300

    deploy no render

--- src/test/java/com/bruno/desafiovotacao/DesafioVotacaoApplicationTests.java
+++ src/test/java/com/bruno/desafiovotacao/DesafioVotacaoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bruno.desafiovotacao;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DesafioVotacaoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.bruno.desafiovotacao;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DesafioVotacaoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
